e20c85f3e4c6325843b5f37c5675687e7d7583e7
KernelDN <diegonatalo@gmail.com>
2020-11-16 23:00:07 -0300

    Update folders and files organization

---
+++ portfolio/js/script.js
@@ -0,0 +1,11 @@
+new fullpage('#fullpage', {
+    sectionsColor: ['', '#4DAA57', '#805D93', '#5299D3', '#EE6352', '#0F7173'],
+    continuousVertical: true,
+    navigation: true,
+    navigationPosition: 'right',
+    slidesNavigation: true,
+});
+
+sal({
+    once: false,
+});
